ea73f668e27f192392b03c6f04177156c7f4ac96
seriousme <hans.klunder@xs4all.nl>
2013-11-04 19:45:56 +0100

    Modified to use Restify

---
+++ readSerial.js
@@ -0,0 +1,64 @@
+// acquire weather data from 868 weather sensors using the IT+ protocol via a JeeLink
+          
+		  
+var com = require("serialport");
+//var comPort='/dev/ttyUSB0';
+var comPort='COM5';
+
+var sensors={};
+// log once every five minutes
+var timeInterval=5000*60*1;
+
+
+module.exports= { 
+	startSerial: function (db){
+		var serialPort = new com.SerialPort(comPort, {
+			baudrate: 57600,
+			parser: com.parsers.readline('\r\n')
+		});
+
+		serialPort.on('open',function() {
+		//console.log('Port open');
+		});
+
+		serialPort.on('data', function(msg) {
+		  var now=new Date();
+		  var newTime=now.getTime();
+		  var datestr=now.toJSON();
+		  console.log(datestr,msg);
+		  // IT+ ID: F0 Temp: 14.8 Humidity: 84 RawData: 9F 05 48
+		  var data=msg.split(' ');
+		  if (data[0]==='IT+')  {
+			var id=data[2];
+			console.log("id",id);
+			if (typeof(sensors[id])=='undefined'){
+				sensors[id]={};
+			}
+			if (typeof(sensors[id].nextTime)=='undefined'){
+				sensors[id].nextTime=0;
+			}
+			
+			if (newTime >= sensors[id].nextTime){
+				if (data[7] != 'L')
+					data[7]='ok';
+				var record={ date: datestr,
+				   id: id,
+				   temp: Number(data[4]),
+				   humid: Number(data[6]),
+				   batt: data[7],
+				   msg: msg,
+				   nextTime: newTime+timeInterval
+				};
+				
+				db.put(datestr,record);
+			}
+		  }
+		});
+	},
+
+}
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 
+
+
+var host = 'localhost';
+var port = 80;
+
+require ('./jsonDateParse');
+var rs=require ('./readSerial');
+
 var levelup=require('level');
 var db = levelup('weatherDB',{ valueEncoding: 'json' });
+rs.startSerial(db);
+
+var ranges={
+	now:{ adjust:function (d){ d.setHours(d.getHours()-12); },
+		  inRange:function(d1,d2) { return true}
+		},
+	day:{ adjust:function (d){ d.setDate(d.getDate()-1); },
+		  inRange:function(d1,d2) { return (d1.getHours()==d2.getHours())}
+		},
+	week:{ adjust:function (d){ d.setDate(d.getDate()-7);},
+		   inRange:function(d1,d2) { return (d1.getDay()==d2.getDay())}
+		},
+	month:{ adjust:function (d){ d.setMonth(d.getMonth()-1); },
+		  inRange:function(d1,d2) { return (d1.getDay()==d2.getDay())}
+		},
+	year:{ adjust:function (d){ d.setFullYear(d.getFullYear()-1);},
+		  inRange:function(d1,d2) { return (d1.getMonth()==d2.getMonth())}
+		},
+};
 
 function setMax(a,i,b){
-	if (typeof a[i] == 'undefined')
+	b=Number(b); //explicit cast to avoid accidents
+	if (typeof(a[i]) == 'undefined')
 		a[i]=b;
 	else
 		if (b > a[i])
@@ -11,83 +39,153 @@ function setMax(a,i,b){
 }
 
 function setMin(a,i,b){
-	if (typeof a[i] == 'undefined')
+	b=Number(b); //explicit cast to avoid accidents
+	if (typeof(a[i]) == 'undefined')
 		a[i]=b;
 	else
 		if (b < a[i])
 			a[i]=b;
 }
-//db.createReadStream({end:2014,limit:2})
 
-function processRecord(items,data){
-	console.log(data);
-	var id=data.id;
-	if (! items[id]){
-	items[id]={
-		id: id,
-		temp:{},
-		humid:{},
-		datapoints: []
-		};
+function setAvg(a,c,i,b){
+	b=Number(b); //explicit cast to avoid accidents
+	if (typeof(a[c]) == 'undefined'){
+		a[c]=1;
+		a[i]=b;
 	}
-
-	setMax(items[id].temp,'max',data.temp);
-	setMin(items[id].temp,'min',data.temp);
-	setMax(items[id].humid,'max',data.humid);
-	setMin(items[id].humid,'min',data.humid);
-	items[id].batt=data.batt;
-	items[id].at=data.date;
-	items[id].temp.current=data.temp;
-	items[id].humid.current=data.humid;
-	items[id].datapoints.push({
-		temp: data.temp,
-		humid: data.humid,
-		at:data.date
-	});
-
+	else{
+		a[i]=(a[c]*a[i]+b)/++a[c];
+	}
 }
 
-function getData(query,cb){
+function setMinMax(result,data){
+	setMax(result.temp,'max',data.temp);
+	setMin(result.temp,'min',data.temp);
+	setMax(result.humid,'max',data.humid);
+	setMin(result.humid,'min',data.humid);
+	setAvg(result.temp,'points','avg',data.temp);
+	setAvg(result.humid,'points','avg',data.humid);
+}
+
+
+function processRecordNow(item,data){
+	if (typeof item.temp=='undefined'){
+		item.temp={};
+		item.humid={};
+	}
+	setMinMax(item,data);
+	item.batt=data.batt;
+	item.at=data.date;
+	item.temp.current=data.temp;
+	item.humid.current=data.humid;
+}
+
+
+function endInterval(result){
+	for (id in result){
+		if (result[id].interval){
+			result[id].datapoints.push(result[id].interval);
+			delete result[id].interval;
+			delete result[id].lastDate;
+		}
+	}
+}
+
+function processRecordInterval(item,data,range){
+	var id=data.id;
+	if (typeof(item.datapoints) == 'undefined'){
+		item.datapoints= [];
+		item.temp={};
+		item.humid={};
+	}
+	
+	if (typeof(item.interval) == 'undefined'){
+		item.interval={
+			temp:{},
+			humid:{}
+		};
+		item.lastDate= data.date;
+	}
+	else{
+		if (! ranges[range].inRange(item.lastDate,data.date)){
+			item.interval.at=data.date;
+			item.datapoints.push(item.interval);
+			item.interval={
+				temp:{},
+				humid:{}
+			};
+			item.lastDate= data.date;
+		}
+	}
+	
+	 // overall min/max
+	setMinMax(item,data);
+	// interval min/max
+	setMinMax(item.interval,data);
+}
+
+
+
+
+function processRecord(items,data,range){
+	if (! items[data.id])
+		items[data.id]={ id: data.id };
+	if (range=='now')
+		processRecordNow(items[data.id],data);
+	else {
+		processRecordInterval(items[data.id],data,range);
+	}
+}
+
+
+function adjustJsonDate(query){
+	var d={};
+	if (! ranges[query.range])
+		query.range='now';
+	if (query.end)
+		d=JSON.parse(query.end);
+	console.log('parsed date',d);
+	if (typeof(d.date)!='object')
+		d=new Date();
+	ranges[query.range].adjust(d);
+	
+	query.start=d.toJSON();
+}
+
+function send(req, res, next){
 	var result={};
+	var query=req.params;
+	adjustJsonDate(query);
+	//db.createReadStream({end:2014,limit:2})
 	db.createReadStream(query)
 		.on('data', function (data) {
-			processRecord(result,data.value);
+			processRecord(result,data.value,query.range);
 		})
 		.on('end', function (){
-			cb(result);
+			// save the last interval
+			endInterval(result);
+			res.end(JSON.stringify(result));
 		});
+	return next();
 }
 
-var connect = require('connect');
-var http = require('http');
+var restify = require('restify');
+var server = restify.createServer();
+
+server.use(restify.queryParser());
+server.get('/weather',send );
+server.get('/weather/:range',send );
+// this one needs to be last
+server.get('/.*',restify.serveStatic({
+  directory: './public',
+  default:'index.html'
+}));
+    
 
 
-var app = connect()
-  .use(connect.logger('dev'))
-  .use(connect.static('public'))
-  .use(connect.query())
-  .use('/weather',function(req, res){
-    getData(req.query,function(data){ res.end(JSON.stringify(data))});
-   })
-  .use(function(req, res){
-    res.writeHead(404, {'content-type':'text/plain'});
-    res.end('404: Page not found\n');
-  });
 
-
-var host = 'localhost';
-var port = 80;
-
-// required for c9.io testing compatibility
-if (process.env.C9_PID)
-{
-    host=process.env.IP;
-    port=process.env.PORT;
-}
-else
-    console.log('Server is running at','http://'+host+':'+port);
-
-http.createServer(app).listen(port,host);
+server.listen(port,host);
+console.log('Server is running at','http://'+host+':'+port);
 
 
 
